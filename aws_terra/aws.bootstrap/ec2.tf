605abcf30030ca710daf4bb3fdef4d4413b191d9
Eddie Cisneros <eddiefcisneros@hotmail.com>
2025-11-01 15:07:47 -0400

    single file deployment of ec2

--- aws_terra/aws.bootstrap/ec2.tf
+++ aws_terra/aws.bootstrap/ec2.tf
@@ -1,4 +1,68 @@
-# creates the bootstrap ec2 instance
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.1"
+    }
+   
+  }
+}
+# ---------------------
+# Data sources
+# ---------------------
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Name"
+    values = ["*Private Subnet*"]
+  }
+}
+
+data "aws_subnet" "subnet" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
+# ---------------------
+# EC2 resource
+# ---------------------
+
+resource "aws_instance" "test_env_ec2" {
+  for_each = data.aws_subnet.subnet
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = each.value.id
+
+  tags = {
+    Name = "test-env-ec2-${each.key}"
+  }
+}
+
+# ---------------------
+# Outputs
+# ---------------------
+
+output "subnet_ids" {
+  description = "IDs of all subnets matched by the filter"
+  value       = [for s in values(data.aws_subnet.subnet) : s.id]
+}
+
+  variable "ami_id" {
+    type = string
+}
+variable "instance_type" {
+  type = string
+}
+
+
+/*
 resource "aws_instance" "test_env_ec2" {
     ami             = "ami-0e46a6a8d36d6f1f2"
     instance_type   = "t2.micro"
@@ -10,7 +74,7 @@ resource "aws_instance" "test_env_ec2" {
     tags = {
       Name = "test Server"
       Creation_time = time_static.JCRS-e-time.rfc3339
-      Creator       = data.external.aws_username.result["name"]
+      #Creator       = data.external.aws_username.result["name"]
 
     }
     user_data = <<-EOF
@@ -22,3 +86,4 @@ resource "aws_instance" "test_env_ec2" {
                 EOF
 }
 
+*/
--- aws_terra/aws.bootstrap/output.tf
+++
@@ -1,9 +0,0 @@
-output "subnet" {
-    value = [for subnet in data.aws_subnet.subnet : subnet.id]
-}
-
-/*
-output "name" {
-  value = data.aws_subnet.pub_subnet[*].id
-}
-*/
